ef4159e57f88f1215e2f161a9043b3fd9e103845
Nguyen Huu Quang Hoa <qhoa2563@outlook.com>
2025-05-05 23:27:50 +0700

    Remove Eureka Server

--- bill-costume-service/src/main/java/com/costumeRental/billcostumeservice/BillCostumeServiceApplication.java
+++ bill-costume-service/src/main/java/com/costumeRental/billcostumeservice/BillCostumeServiceApplication.java
@@ -2,10 +2,8 @@ package com.costumeRental.billcostumeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BillCostumeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BillCostumeServiceApplication.class, args);
--- supplier-service/src/main/java/com/costumeRental/supplierservice/SupplierServiceApplication.java
+++ supplier-service/src/main/java/com/costumeRental/supplierservice/SupplierServiceApplication.java
@@ -2,10 +2,8 @@ package com.costumeRental.supplierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SupplierServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SupplierServiceApplication.class, args);
